fac6de55071b086afa4e1fb1232a9186b8d6e3cc
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-09-21 01:56:20 -0700

    Leaderboard is up and local storage works too.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Define variables
-const startButton = document.getElementById("start");
+const startButton = document.getElementById("start-button");
 const quizSection = document.getElementById("quiz");
 const resultSection = document.getElementById("result");
-const landingPageSection = document.getElementById("landing-page");
+const landingPageSection = document.querySelector(".landing-page");
 const scoreDisplay = document.getElementById("score");
 const choices = document.querySelectorAll(".choice");
 const submitButton = document.getElementById("submit");
@@ -17,6 +17,21 @@ const questions = [
     question: "Testing",
     answers: ["1", "2", "3", "4"],
     correct: 0, // Index of the correct answer
+  },
+  {
+    question: "Testing",
+    answers: ["1", "2", "3", "4"],
+    correct: 3, // Index of the correct answer
+  },
+  {
+    question: "Testing",
+    answers: ["1", "2", "3", "4"],
+    correct: 2, // Index of the correct answer
+  },
+  {
+    question: "Testing",
+    answers: ["1", "2", "3", "4"],
+    correct: 1, // Index of the correct answer
   },
 ];
 
@@ -47,10 +62,10 @@ function showQuestion() {
     quizSection.style.display = "block";
     resultSection.style.display = "none";
     const questionData = questions[currentQuestion];
-    document.querySelector("h2").textContent = `Question ${
+    document.querySelector("#quiz h2").textContent = `Question ${
       currentQuestion + 1
     }`;
-    document.querySelector("p").textContent = questionData.question;
+    document.querySelector("#quiz p").textContent = questionData.question;
     for (let i = 0; i < choices.length; i++) {
       choices[i].textContent = questionData.answers[i];
       choices[i].addEventListener("click", checkAnswer);
@@ -82,12 +97,71 @@ function endQuiz() {
   scoreDisplay.textContent = score;
 }
 
-// Placeholder function for submitting scores
+// Function to submit the score
 function submitScore() {
-  // Implement your code for submitting scores here
-  alert("Score submitted!"); // This is just a placeholder
+  const playerName = prompt("Enter your name:"); // Prompt user for their name
+  if (!playerName) {
+    return; // Do not save the score if no name is provided
+  }
+
+  const playerScore = {
+    playerName,
+    score, // Use the global score variable
+  };
+
+  // Retrieve existing scores from local storage and add the new score
+  const scores = JSON.parse(localStorage.getItem("scores")) || [];
+  scores.push(playerScore);
+
+  // Store the updated scores back in local storage
+  localStorage.setItem("scores", JSON.stringify(scores));
+
+  alert("Score submitted successfully!");
 }
+
+// Function to retrieve scores from local storage
+function getScores() {
+  // Retrieve scores from local storage (assuming scores are stored as an array of objects)
+  const scores = JSON.parse(localStorage.getItem("scores")) || [];
+  // Sort scores in descending order (highest score first)
+  scores.sort((a, b) => b.score - a.score);
+  return scores;
+}
+
+// Function to display scores on the leaderboard page
+function displayScores() {
+  const leaderboardTable = document.querySelector("tbody");
+  const scores = getScores();
+
+  // Clear existing leaderboard data
+  leaderboardTable.innerHTML = "";
+
+  // Loop through scores and add them to the leaderboard
+  scores.forEach((score, index) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+            <td>${index + 1}</td>
+            <td>${score.playerName}</td>
+            <td>${score.score}</td>
+        `;
+    leaderboardTable.appendChild(row);
+  });
+}
+
+// Function for the "View Leaderboard" button click
+document
+  .getElementById("leaderboard-button")
+  .addEventListener("click", function () {
+    // Toggle the visibility of the leaderboard section
+    const leaderboardSection = document.getElementById("leaderboard");
+    leaderboardSection.style.display =
+      leaderboardSection.style.display === "none" ? "block" : "none";
+
+    // Call the function to display leaderboard data (you may need to modify this function)
+    displayScores();
+  });
 
 // Event listeners
 startButton.addEventListener("click", startQuiz);
 submitButton.addEventListener("click", submitScore);
+window.addEventListener("load", displayScores);
